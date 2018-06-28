5288cb4eb54271fd0f9fd8ac28a0672146a03cc5
FireFox321 <40366139+FireFox321@users.noreply.github.com>
2018-06-28 10:28:32 -0300

    botao tela cadastro

--- MyMarket/src/pages/tela-cadastro/tela-cadastro.ts
+++ MyMarket/src/pages/tela-cadastro/tela-cadastro.ts
@@ -1,3 +1,4 @@
+import { TelaCadastroClientePage } from './../../../../src/pages/tela-cadastro-cliente/tela-cadastro-cliente';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -20,6 +21,10 @@ export class TelaCadastroPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TelaCadastroPage');
+  }
+
+  proxpagina(){
+    this.navCtrl.push(TelaCadastroClientePage);
   }
 
 }
