f07786d5bdc9d8041e69387b3dd1f5ead7cd2836
FireFox321 <40366139+FireFox321@users.noreply.github.com>
2018-07-12 07:54:06 -0300

    Login agora funcionando.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController, Loading } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '../../../node_modules/@angular/forms';
+import { AutenticacaoProvider } from '../../providers/autenticacao/autenticacao';
+import { TelaInicioPage } from '../tela-inicio/tela-inicio';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +18,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public autentica: AutenticacaoProvider,
+    private loading: LoadingController,
+    private alertCtrl: AlertController) {
+
+      let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+      this.loginForm = this.formBuilder.group({
+        email: ['', [Validators.compose([Validators.required, Validators.pattern(emailRegex)])]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+      });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+
+
+  logar() {
+    let loading: Loading = this.showloading();
+    let formCliente = this.loginForm.value;
+
+    this.autentica.entrarComEmail({
+      email: formCliente.email,
+      senha: formCliente.senha
+    }).then((logado: boolean) => {
+      if (logado) {
+        this.navCtrl.setRoot(TelaInicioPage);
+        loading.dismiss();
+        this.alertShow();
+      }
+    }).catch((error: any) => {
+      console.log(error);
+      loading.dismiss();
+      this.alertErroShow(error)
+    });
+  }
+
+  private showloading(): Loading {
+    let loading: Loading = this.loading.create({
+      content: "Por favor aguarde..."
+    })
+    loading.present();
+    return loading;
+  }
+
+  private alertShow() {
+    this.alertCtrl.create({
+      title: "Carregado com sucesso!",
+      message: "Toque em OK e vamos iniciar! :D",
+      buttons: [{
+        text: "OK",
+        role: "null"
+      }],
+      cssClass: "$alert-input-placeholder-color",
+
+
+    }).present();
+  }
+
+  private alertErroShow(message:string): void{
+    message = "Error ao entrar ! Verifique seu email ou senha."
+    this.alertCtrl.create ({
+      message: message,
+      buttons: [{
+        text: "OK",
+        role: null
+      }]
+    }).present();
   }
 
 }
--- src/providers/autenticacao/autenticacao.ts
+++ src/providers/autenticacao/autenticacao.ts
@@ -1,15 +1,16 @@
 import { AngularFireAuth } from "angularfire2/auth";
 import { Injectable } from '@angular/core';
+import { BaseService } from "../base.services";
 
 
 @Injectable()
-export class AutenticacaoProvider {
-  handlePromiseError(arg0: any): any {
-    throw new Error("Erro na autenticação");
-  }
+export class AutenticacaoProvider extends BaseService {
+
+
   constructor(
     public autentica: AngularFireAuth
   ) {
+    super();
     console.log(' AutenticacaoProvider aberto ');
   }
 
@@ -17,6 +18,13 @@ export class AutenticacaoProvider {
   criarAutenticacaoCliente(cliente: {email:string, senha:string}): Promise<firebase.User>{
     return this.autentica.auth.createUserWithEmailAndPassword(cliente.email , cliente.senha)
     .catch(this.handlePromiseError);
+  }
+
+  entrarComEmail(cliente: {email:string, senha:string}): Promise<boolean>{
+    return this.autentica.auth.signInWithEmailAndPassword(cliente.email, cliente.senha)
+    .then((aut: firebase.auth.UserCredential) =>{
+      return aut.user != null;
+    }).catch(this.handlePromiseError);
   }
 
 
